6df38d473b8856a00251d9963ce9997e60309810
Jhonatan <jhonatantiradotiradodeep@gmail.com>
2019-11-28 00:23:59 -0500

    Update role_id and category_id. ClearDB AutoIncrement by 10 for replication

---
+++ DropAllTables.sql
@@ -0,0 +1,13 @@
+DROP TABLE sale_order_detail;
+DROP TABLE sale_order;
+DROP TABLE purchase_order_detail;
+DROP TABLE purchase_order;
+DROP TABLE product;
+DROP TABLE category;
+DROP TABLE currency;
+DROP TABLE customer;
+DROP TABLE employee;
+DROP TABLE role;
+DROP TABLE store;
+DROP TABLE provider;
+DROP TABLE versioninfo;
